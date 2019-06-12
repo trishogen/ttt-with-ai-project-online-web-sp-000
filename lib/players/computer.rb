540f12810710b2b5f4a682c20d30e6e96228c839
Trish Ogen <patriciaogen@gmail.com>
2019-06-12 01:56:05 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,18 +2,31 @@ module Players
 
   class Computer < Player
 
-    def move(board)
+    def old_move(board)
       (board.cells.index { |x| x == " " } + 1).to_s
     end
   end
 
-  def win_C
-    puts Game.WIN_COMBINATIONS
+  def opp_token
+    self.token == 'X' ? 'O' : 'X'
   end
-  #for each win combination check if 2/3 spots contain oponents token
-    #if yes move to third spot
-  #for each win combination check if 1/3 spots contain oponents token and other two spots are empty
-    #if yes move there
-  #else move to random open spot
+
+  def move(board)
+    #set to any open position as default
+    move_to_make = board.cells.index { |x| x == " " }
+
+    Game.WIN_COMBINATIONS.find do |v|
+       values = board.cells.values_at(v[0],v[1],v[2])
+       #set move to a combo with 1 opp_token and 2 empty spaces
+       if values.count(opp_token) == 1 && values.count(' ') == 2
+         move_to_make = v.find do |item| board.cells[item] == ' '
+       end
+       #set move to a combo with 1 opp_token and 2 empty spaces
+       if values.count(opp_token) == 2 && values.count(' ') == 1
+         move_to_make = v.find do |item| board.cells[item] == ' '
+       end
+     end
+     (move_to_make + 1).to_s
+  end
 
 end
