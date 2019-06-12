cea1dc35c5665aeeed6e0cfe3b18f23ce050af78
Trish Ogen <patriciaogen@gmail.com>
2019-06-12 02:06:52 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 class Game
   attr_accessor :board, :player_1, :player_2, :game_type, :first_player,
-  attr_reader WIN_COMBINATIONS
-  
+  attr_reader :WIN_COMBINATIONS
+
   WIN_COMBINATIONS = [
                       [0,1,2], [3,4,5], [6,7,8], # across
                       [0,3,6], [1,4,7], [2,5,8], # down
